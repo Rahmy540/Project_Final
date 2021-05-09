44a72c3ce034a95976266ad5666388d154f03d39
Rahmy540 <rahmyrahmy81@gmail.com>
2021-05-09 22:28:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/movieappapi/networks/Consts.java
@@ -0,0 +1,12 @@
+package com.example.movieappapi.networks;
+
+public class Consts {
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String APIKEY = "d8a44b3af1d1ac77b9b8c60efa9b3aab";
+    public static final String LANGUAGE = "en-US";
+    public static final String PAGE = "1";
+    public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w500/";
+    public static final String APIERROR = "DEBUG:API ERROR";
+
+}
